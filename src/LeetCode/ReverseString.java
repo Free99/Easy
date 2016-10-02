04f7d68dff7c7a707d8be81bb881f57f48e935cc
Tianshan <tianshan.cui@gmail.com>
2016-10-01 23:07:47 -0700

    344. Reverse String

---
+++ src/LeetCode/ReverseString.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/1/16.
+ */
+public class ReverseString {
+    public String reverseString(String s){
+        // basic edge case handling
+        if (s == null || s.length() == 0) return "";
+
+        // better change it to char array
+        char[] word = s.toCharArray();
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            char c = word[i];
+            word[i++] = word[j];
+            word[j--] = c;
+        }
+
+        return String.valueOf(word);
+    }
+}
