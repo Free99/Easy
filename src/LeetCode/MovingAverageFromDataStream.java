c2026340b58ca53d7ddb30e857d5d815c4cf3560
Tianshan <tianshan.cui@gmail.com>
2016-10-01 23:04:06 -0700

    346. Moving Average from Data Stream

---
+++ src/LeetCode/MovingAverageFromDataStream.java
@@ -0,0 +1,29 @@
+package LeetCode;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+/**
+ * Created by Tianshan on 10/1/16.
+ */
+public class MovingAverageFromDataStream {
+    private int size;
+    private double sum;
+    private Queue<Integer> q;
+
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        this.size = size;
+        sum = 0;
+        q = new LinkedList<>();
+    }
+
+    public double next(int val) {
+        if (q.size() == size) {
+            sum -= q.poll();
+        }
+        sum += val;
+        q.offer(val);
+        return sum / q.size();
+    }
+}
