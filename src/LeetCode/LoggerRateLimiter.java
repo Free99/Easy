d7c241ea5c2b93ff264c2bfd55362d4e01137bc5
Tianshan <tianshan.cui@gmail.com>
2016-10-01 23:04:49 -0700

    359. Logger Rate Limiter

---
+++ src/LeetCode/LoggerRateLimiter.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Tianshan on 10/1/16.
+ */
+public class LoggerRateLimiter {
+    Map<String, Integer> map;
+
+    /** Initialize your data structure here. */
+    public Logger() {
+        map = new HashMap<>();
+    }
+
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+     If this method returns false, the message will not be printed.
+     The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (timestamp < map.getOrDefault(message, 0)) return false;
+        map.put(message, timestamp + 10);
+        return true;
+    }
+}
