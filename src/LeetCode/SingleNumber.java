a3e1511e755254c7389f702b4255117c1f88cded
Tianshan <tianshan.cui@gmail.com>
2016-10-02 23:14:54 -0700

    136. Single Number

---
+++ src/LeetCode/SingleNumber.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/2/16.
+ */
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res = res ^ nums[i];
+        }
+        return res;
+    }
+}
