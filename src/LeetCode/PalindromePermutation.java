a15867fc2d44fac6cf1383358993755aa32f66cc
Tianshan <tianshan.cui@gmail.com>
2016-10-02 23:06:03 -0700

    266. Palindrome Permutation

---
+++ src/LeetCode/PalindromePermutation.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Tianshan on 10/2/16.
+ */
+public class PalindromePermutation {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!set.add(c)) {
+                set.remove(c);
+            }
+        }
+        return set.size() < 2;
+    }
+}
