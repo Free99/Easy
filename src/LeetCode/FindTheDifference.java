150aeec1f26dd307583b4fc8104e33005b8b3d09
Tianshan <tianshan.cui@gmail.com>
2016-10-03 23:16:15 -0700

    389. Find the Difference

---
+++ src/LeetCode/FindTheDifference.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/3/16.
+ */
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        int n = t.length() - 1;
+        char c = t.charAt(n);
+        for (int i = 0; i < n; i++) {
+            c ^= s.charAt(i);
+            c ^= s.charAt(i);
+        }
+        return c;
+    }
+}
