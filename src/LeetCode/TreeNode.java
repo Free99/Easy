08c34de12b312ffc0cf9bd5a1ad8d7678f984849
Tianshan <tianshan.cui@gmail.com>
2016-10-03 23:05:31 -0700

    TreeNode

---
+++ src/LeetCode/TreeNode.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/3/16.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
