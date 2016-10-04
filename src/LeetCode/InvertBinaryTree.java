443012f5025d05afd9a1b510710faf60a43d6084
Tianshan <tianshan.cui@gmail.com>
2016-10-04 08:00:50 -0700

    226. Invert Binary Tree

---
+++ src/LeetCode/InvertBinaryTree.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/4/16.
+ */
+public class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode left = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
+        return root;
+    }
+}
