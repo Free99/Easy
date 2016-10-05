9f29b5d9a95483d1d5928dd0c8eb3e176e626150
Tianshan <tianshan.cui@gmail.com>
2016-10-05 10:50:03 -0700

    404. Sum of Left Leaves

---
+++ src/LeetCode/SumOfLeftLeaves.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class SumOfLeftLeaves {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) return 0;
+        int res = 0;
+        if (root.left != null) {
+            // this is a left leaf
+            if (root.left.left == null && root.left.right == null) res += root.left.val;
+            else res += sumOfLeftLeaves(root.left);
+        }
+        if (root.right != null) {
+            res += sumOfLeftLeaves(root.right);
+        }
+        return res;
+    }
+}
