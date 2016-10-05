ea679274787ffd5d26db501a862e16c60063a9fa
Tianshan <tianshan.cui@gmail.com>
2016-10-05 13:53:00 -0700

    242. Valid Anagram

--- src/LeetCode/ValidAnagram.java
+++ src/LeetCode/ValidAnagram.java
@@ -8,7 +8,7 @@ public class ValidAnagram {
         if (s.length() != t.length()) return false;
         int[] map = new int[26]; // only lowercase alphabets
         for (int i = 0; i < s.length(); i++) map[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) if (--map[s.charAt(i) - 'a'] < 0) return false;
+        for (int i = 0; i < t.length(); i++) if (--map[t.charAt(i) - 'a'] < 0) return false;
         return true;
     }
 }
