3556f5bba83ca506620d63af6d7f3add0b2308f4
Tianshan <tianshan.cui@gmail.com>
2016-10-05 13:44:33 -0700

    Interval

---
+++ src/LeetCode/Interval.java
@@ -0,0 +1,11 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class Interval {
+    int start;
+    int end;
+    Interval() { start = 0; end = 0; }
+    Interval(int s, int e) { start = s; end = e; }
+}
