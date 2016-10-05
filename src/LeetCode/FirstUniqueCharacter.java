4d70b73357e6e67401ca3b814f3a761579433116
Tianshan <tianshan.cui@gmail.com>
2016-10-05 14:44:16 -0700

    387. First Unique Character in a String

---
+++ src/LeetCode/FirstUniqueCharacter.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class FirstUniqueCharacter {
+    public int firstUniqChar(String s) {
+        int[] map = new int[256];
+        for (int i = 0; i < s.length(); i++) map[s.charAt(i)]++;
+        for (int i = 0; i < s.length(); i++) if (s.charAt(i) == 1) return i;
+        return -1;
+    }
+}
