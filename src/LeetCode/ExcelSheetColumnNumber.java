40057e64c46d37c5d9d05588fc5dabb01406a3ba
Tianshan <tianshan.cui@gmail.com>
2016-10-05 13:35:18 -0700

    171. Excel Sheet Column Number

---
+++ src/LeetCode/ExcelSheetColumnNumber.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class ExcelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        return s.length() == 0 ? 0 : titleToNumber(s.substring(0, s.length() - 1)) * 26 + s.charAt(s.length() - 1) - 'A' + 1;
+    }
+}
