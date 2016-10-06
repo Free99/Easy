bdaf4c2f851db42bb0343db0fab91768964db36b
Tianshan <tianshan.cui@gmail.com>
2016-10-06 14:09:13 -0700

    249. Group Shifted Strings

---
+++ src/Easy/GroupShiftedStrings.java
@@ -0,0 +1,34 @@
+package Easy;
+
+import java.util.*;
+
+/**
+ * Created by Tianshan on 10/6/16.
+ */
+public class GroupShiftedStrings {
+    public List<List<String>> groupStrings(String[] strings) {
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strings) {
+            int offset = str.charAt(0) - 'a';
+            String key = "";
+            for (int i = 0; i < str.length(); i++) {
+                char c = (char) (str.charAt(i) - offset);
+                if (c < 'a') {
+                    c += 26;
+                }
+                key += c;
+            }
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        for (String key : map.keySet()) {
+            List<String> list = map.get(key);
+            Collections.sort(list);
+            res.add(list);
+        }
+        return res;
+    }
+}
