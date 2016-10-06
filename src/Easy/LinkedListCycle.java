87cb053a9830e009c3a3ad6012667f0aa0cad42b
Tianshan <tianshan.cui@gmail.com>
2016-10-06 13:54:12 -0700

    141. Linked List Cycle

---
+++ src/Easy/LinkedListCycle.java
@@ -0,0 +1,18 @@
+package Easy;
+
+/**
+ * Created by Tianshan on 10/6/16.
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode walker = head;
+        ListNode runner = head;
+        while (runner.next != null && runner.next.next != null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if (walker == runner) return true;
+        }
+        return false;
+    }
+}
