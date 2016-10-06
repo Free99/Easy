ad76316832173580368099c7547cda27b0189aa8
Tianshan <tianshan.cui@gmail.com>
2016-10-05 21:26:21 -0700

    231. Power of Two

---
+++ src/LeetCode/PowerOfTwo.java
@@ -0,0 +1,10 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        return (n > 0) & ((n & (n - 1)) == 0);
+    }
+}
