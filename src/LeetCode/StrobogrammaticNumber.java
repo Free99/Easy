055d206cbed3018b17859e4a1b97b25d8dae523c
Tianshan <tianshan.cui@gmail.com>
2016-10-05 22:16:58 -0700

    246. Strobogrammatic Number

---
+++ src/LeetCode/StrobogrammaticNumber.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class StrobogrammaticNumber {
+    public boolean isStrobogrammatic(String num) {
+        // we need to check the situation when i == j
+        for (int i = 0, j = num.length() - 1; i <= j; i++, j--) {
+            if (!"00 11 696 88".contains(num.charAt(i) + "" + num.charAt(j))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
