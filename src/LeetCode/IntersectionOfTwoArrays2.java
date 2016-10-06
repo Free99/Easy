1a7a3f6be68a6b748cd6d5de5ea17121403afd84
Tianshan <tianshan.cui@gmail.com>
2016-10-05 20:24:22 -0700

    350. Intersection of Two Arrays II

---
+++ src/LeetCode/IntersectionOfTwoArrays2.java
@@ -0,0 +1,34 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class IntersectionOfTwoArrays2 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> resList = new ArrayList<>();
+        // two pointers
+        for (int i = 0, j = 0; i < nums1.length && j < nums2.length; ) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] == nums2[j]) {
+                resList.add(nums1[i]);
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        int[] res = new int[resList.size()];
+        int i = 0;
+        for (int num : resList) {
+            res[i++] = num;
+        }
+        return res;
+    }
+}
