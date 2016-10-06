d254b6272b75ba5b655777657802ce448bb6395b
Tianshan <tianshan.cui@gmail.com>
2016-10-06 16:13:13 -0700

    111. Minimum Depth of Binary Tree

--- src/Easy/MinimumDepthOfBinaryTree.java
+++ src/Easy/MinimumDepthOfBinaryTree.java
@@ -7,6 +7,6 @@ public class MinimumDepthOfBinaryTree {
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
         if (root.left != null && root.right != null) return 1 + Math.min(minDepth(root.left), minDepth(root.right));
-        return 1 + minDepth(root.left) + minDepth(root.right); // there must exist a null node for left or right;
+        else return 1 + minDepth(root.left) + minDepth(root.right); // there must exist a null node for left or right;
     }
 }
