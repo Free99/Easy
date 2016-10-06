3b5fc2ceec5ba268673ba0e223728bc0fc461d91
Tianshan <tianshan.cui@gmail.com>
2016-10-06 14:42:21 -0700

    102. Binary Tree Level Order Traversal

---
+++ src/Easy/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,30 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Created by Tianshan on 10/6/16.
+ */
+public class BinaryTreeLevelOrderTraversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new ArrayList<>();
+            for (int i = 0; i < levelNum; i++) {
+                TreeNode node = queue.poll();
+                subList.add(node.val);
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+            res.add(subList);
+        }
+        return res;
+    }
+}
