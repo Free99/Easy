1ae74ae7aa5cb83e8075843c549684f491c23ba8
Tianshan <tianshan.cui@gmail.com>
2016-10-05 17:46:31 -0700

    217. Contains Duplicate

---
+++ src/LeetCode/ContainsDuplicate.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Tianshan on 10/5/16.
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) return true;
+        }
+        return false;
+    }
+}
