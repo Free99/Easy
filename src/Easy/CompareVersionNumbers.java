b2d6705ef11fe61be33c0ed30be3fb091a987814
Tianshan <tianshan.cui@gmail.com>
2016-10-07 15:18:09 -0700

    165. Compare Version Numbers

---
+++ src/Easy/CompareVersionNumbers.java
@@ -0,0 +1,31 @@
+package Easy;
+
+/**
+ * Created by Tianshan on 10/7/16.
+ */
+public class CompareVersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        if (version1 == null || version2 == null) {
+            return 0;
+        }
+        if (version1 == null || version2 == null) {
+            return version1 == null ? -1 : 1;
+        }
+
+        String[] list1 = version1.split("\\.");
+        String[] list2 = version2.split("\\.");
+
+        int i = 0;
+        while (i < list1.length || i < list2.length) {
+            int a = i < list1.length ? Integer.valueOf(list1[i]) : 0;
+            int b = i < list2.length ? Integer.valueOf(list2[i]) : 0;
+            if (a < b) {
+                return -1;
+            } else if (a > b) {
+                return 1;
+            }
+            i++;
+        }
+        return 0;
+    }
+}
