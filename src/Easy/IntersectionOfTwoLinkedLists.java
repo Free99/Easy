581784bfea4566f741bb0dec717e1dd3569e27dd
Tianshan <tianshan.cui@gmail.com>
2016-10-07 10:44:27 -0700

    160. Intersection of Two Linked Lists

---
+++ src/Easy/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,17 @@
+package Easy;
+
+/**
+ * Created by Tianshan on 10/7/16.
+ */
+public class IntersectionOfTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+        return a;
+    }
+}
