e3741886eb615313e37100eda4935ac5a9e12823
Tianshan <tianshan.cui@gmail.com>
2016-10-07 10:56:12 -0700

    203. Remove Linked List Elements

---
+++ src/Easy/RemoveLinkedListElements.java
@@ -0,0 +1,12 @@
+package Easy;
+
+/**
+ * Created by Tianshan on 10/7/16.
+ */
+public class RemoveLinkedListElements {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
+}
