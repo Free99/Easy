136141ea1cf6016cb757c4fb627073f0c58fd7af
Tianshan <tianshan.cui@gmail.com>
2016-10-07 14:47:29 -0700

    8. String to Integer (atoi)

---
+++ src/Easy/StringToInteger.java
@@ -0,0 +1,23 @@
+package Easy;
+
+/**
+ * Created by Tianshan on 10/7/16.
+ */
+public class StringToInteger {
+    public int myAtoi(String str) {
+        if (str == null || str.length() == 0) return 0;
+        int sign = 1, base = 0, i = 0;
+        // trim
+        while (str.charAt(i) == ' ') i++;
+        if (str.charAt(i) == '+' || str.charAt(i) == '-') {
+            sign = str.charAt(i++) == '-' ? -1 : 1;
+        }
+        while (i < str.length() && str.charAt(i) <= '9' && str.charAt(i) >= '0') {
+            if (base > Integer.MAX_VALUE / 10 || (base == Integer.MAX_VALUE / 10 && (str.charAt(i) - '0') > 7)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            base = base * 10 + (str.charAt(i++) - '0');
+        }
+        return base * sign;
+    }
+}
