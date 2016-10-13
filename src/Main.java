0becdae79faef9ed471e36115d2df535ea852ad3
free99 <tianshan.cui@gmail.com>
2016-10-12 23:01:03 -0700

    418. Sentence Screen Fitting

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
-import Easy.AddStrings;
+import Medium.SentenceScreenFitting;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello world!");
-        AddStrings a = new AddStrings();
-        System.out.println(a.addStrings("9929", "1321"));
+        SentenceScreenFitting a = new SentenceScreenFitting();
+        String[] words = {"I", "had", "apple", "pie"};
+        System.out.println(a.wordsTyping(words, 4, 5));
     }
 }
