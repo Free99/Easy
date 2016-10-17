4875004b805d92e9af4fc565e72fcbc5b26dbfdc
free99 <tianshan.cui@gmail.com>
2016-10-17 16:02:46 -0700

    TreeNode

--- src/Easy/TreeNode.java
+++ src/Easy/TreeNode.java
@@ -4,8 +4,8 @@ package Easy;
  * Created by Tianshan on 10/3/16.
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
     TreeNode(int x) { val = x; }
 }
